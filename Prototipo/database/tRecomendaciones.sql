1513a6124f332bd588ef8f8edda5086e0470dc99
jeremyiraheta <47946244+jeremyiraheta@users.noreply.github.com>
2019-10-26 11:19:59 -0600

    trigger recomendaciones

---
+++ Prototipo/database/tRecomendaciones.sql
@@ -0,0 +1,30 @@
+create trigger tRecomendaciones
+on VALORACIONES
+after UPDATE
+as
+begin
+	declare @iduser varchar(50)
+	declare @idplatillo int	
+	declare @valoracion float
+	select @iduser=ID_USUARIO,@idplatillo=ID_PLATILLOS,@valoracion=RATE from inserted
+	declare @id varchar(50)
+	declare @rate float
+	declare each_row cursor 
+	for select ID_USUARIO, RATE from VALORACIONES where ID_PLATILLOS=@idplatillo and floor(RATE)= FLOOR(@valoracion)
+	open each_row
+	fetch next from each_row into @id,@rate
+	WHILE @@FETCH_STATUS = 0
+		BEGIN			
+			if Exists(select * from RECOMENDACIONES where (ID_USUARIOA = @iduser and ID_USUARIOB =@id))
+				update RECOMENDACIONES set COINCIDENCIAS += 1 where (ID_USUARIOA = @iduser and ID_USUARIOB =@id)
+			else if Exists(select * from RECOMENDACIONES where (ID_USUARIOA = @id and ID_USUARIOB =@iduser))
+				update RECOMENDACIONES set COINCIDENCIAS += 1 where (ID_USUARIOA = @id and ID_USUARIOB =@iduser)
+			else
+				insert into RECOMENDACIONES values(@iduser,@id,1)
+			fetch next from each_row into @id,@rate        
+		END;
+ 
+	CLOSE each_row;
+ 
+	DEALLOCATE each_row;
+end
--- Prototipo/database/tRestaurantes.sql
+++ Prototipo/database/tRestaurantes.sql
@@ -9,6 +9,6 @@ begin
 		select @id=ID_RESTAURANTES from inserted
 	if Exists(select * from deleted)
 		select @id=ID_RESTAURANTES from deleted
-	select @avg=AVG(RATE) from RESTAURANTES where ID_RESTAURANTES = @id
+	select @avg=AVG(RATE) from PLATILLOS where ID_RESTAURANTES = @id
 	update RESTAURANTES set RATE = @avg where ID_RESTAURANTES = @id
 end
