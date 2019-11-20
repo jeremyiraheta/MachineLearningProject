f91ddbd042d48b8a202210d3e7dc3805f5680575
jeremyiraheta <47946244+jeremyiraheta@users.noreply.github.com>
2019-11-20 11:34:41 -0600

    ml

--- WebService/CrudService.svc.cs
+++ WebService/CrudService.svc.cs
@@ -368,7 +368,7 @@ namespace WebService
             return (platillos.Count > 0) ? platillos : sp_UltimosPlatillos(0, 10); 
         }
         public List<Platillos> sp_RecomendarProductosPersonalizado(string user)
-        {
+        {//select v1.id_usuario, v2.id_usuario,count(v1.rate) coincidencias from valoraciones v1 inner join valoraciones v2 on v1.id_platillos = v2.id_platillos and v1.id_usuario != v2.id_usuario and v1.rate = v2.rate group by v1.id_usuario,v2.id_usuario
             DataSet ds = Select($"select top 10 id_usuarioa,id_usuariob from recomendaciones where id_usuarioa='{user}' or id_usuariob='{user}' order by coincidencias");
             List<Platillos> platillos = new List<Platillos>();
             Dictionary<int, int> unificated = new Dictionary<int, int>();
